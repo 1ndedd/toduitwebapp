02caa916fe76fb6870b29c720426dd4dd7d37c93
1ndedd <99084465+1ndedd@users.noreply.github.com>
2023-06-14 12:02:14 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//server/index.js
-//
-//Setting up the server
-
+/**server/index.js
+ * Setting up the node + express server
+*/
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; // port num
 
 const app = express();
 
+//listen to server of port 3001
 app.listen(PORT, () => {
 	console.log(`Server listening to port ${PORT}`);
 	});
