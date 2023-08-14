f8eefacdb674045c0900fd1961e72d592e5217dc
1ndedd <99084465+1ndedd@users.noreply.github.com>
2023-08-14 10:38:11 +0100

    able to do GET and POST request

--- server/index.js
+++ server/index.js
@@ -6,8 +6,19 @@ const express = require("express");
 const PORT = process.env.PORT || 3001; // port num
 
 const app = express();
+app.use(express.json());
+
+const db = require("./models");	//fetch tables
+
+//Routers
+const taskRoute = require("./routes/Task");	//get task route
+app.use("/Task", taskRoute); //parse
 
 //listen to server of port 3001
-app.listen(PORT, () => {
-	console.log(`Server listening to port ${PORT}`);
-	});
+//check if tables exist
+db.sequelize.sync().then( () => {
+	app.listen(PORT, () => {
+		console.log(`Server listening to port ${PORT}`);
+		});
+});
+
