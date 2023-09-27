091781952f143521ae0244e1710cb9d90d3b44a9
naufal <wanazlan@lancaster.ac.uk>
2023-09-27 14:39:12 +0100

    fixed yup form validation issues

--- client/src/pages/CreateTask.js
+++ client/src/pages/CreateTask.js
@@ -1,41 +1,66 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Form, Field, ErrorMessage, Formik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 function CreateTask() {
-    const formik = useFormik({
-        initialValues: {
-            id:"",
-            task: ""
-        },
-        onSubmit: (values) => {
-            console.log("onSubmit", values);
-        },
-    })
+    const initialValues = {
+            id: '',
+            task: "",
+        };
+        
+    const validationSchema =  Yup.object().shape({
+            id: Yup.number()
+            .required('Required'),
+            
+            task: Yup.string()
+            .required("Required"),
+        });
+        
+    const onSubmit = (values) => {
+            axios.post("http://localhost:3001/Task", values).then((res) => {
+                console.log("onSubmit", values);
+            });
+        };
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div className="field">
-                <input 
-                    name="id"
-                    placeholder="Insert ID (integer number)"
-                    id = "id"
-                    value={formik.values.id}
-                    onChange={formik.handleChange}
-                />
-            </div>
-            
-            <div className="field">  
-                <input 
-                    name="task;"
-                    placeholder="What Task Is On Your Mind?"
-                    task = "task"
-                    value={formik.values.task}
-                    onChange={formik.handleChange}
-                />
-            </div>
+    <div className= "TaskLayout">
+        <Formik
+            initialValues = {initialValues} 
+            onSubmit={onSubmit} 
+            validationSchema = {validationSchema}
+        >
+            {({values, errors, touched }) => (
+            <Form>
+                <div className="idInput">
+                    <label>Input ID number:</label>
+                    <Field 
+                        name="id"
+                        placeholder="Insert ID (integer number)"
+                    />
+                        {touched.id && errors.id ? (
+                            <div>{errors.id}</div>
+                        ) : null}
+                </div>
 
-            <button type="submit">submit</button>
-        </form>
+                <div className="taskInput">
+                    <label>Input task:</label>
+                    <Field 
+                        name="task"
+                        placeholder="What Task Is On Your Mind?"
+                        task ="task"
+                        // value={values.task}
+                        // onChange={Formik.handleChange}
+                    />
+                        {touched.task && errors.task ? (
+                            <div>{errors.task}</div>
+                        ) : null}
+                </div>
+                <button type="submit">Submit</button>
+            </Form>
+            )}
+        </Formik>
+    </div>
     )
 }
 
